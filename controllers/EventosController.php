5b9fa8a837de49b19d4b4a49c947562cffc453dd
PedroCaaveiro <acaaveir@yahoo.es>
2025-06-18 12:30:31 +0200

    cambios

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -5,6 +5,11 @@ namespace Controllers;
 
 
 use MVC\Router;
+use Model\Categoria;
+use Model\Dia;
+use Model\Hora;
+use Model\Evento;
+
 
 class EventosController{
 
@@ -21,9 +26,34 @@ $router->render('admin/eventos/index',[
 public static function crear(Router $router){
 
    $alertas =[];
+
+$categorias = Categoria::all('ASC');
+$dias = Dia::all('ASC');
+$horas = Hora::all('ASC');
+
+$evento = new Evento();
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $evento->sincronizar($_POST);
+    $alertas = $evento->validar();
+    if (empty($alertas)) {
+        $resultado = $evento->guardar();
+
+        if ($resultado) {
+            header('Location:'.BASE_URL. 'admin/eventos');
+        }
+    }
+}
+
+
 $router->render('admin/eventos/crear',[
     'titulo'=> 'Registrar Eventos',
-    'alertas' => $alertas
+    'alertas' => $alertas,
+    'categorias' => $categorias,
+    'dias' => $dias,
+    'horas' => $horas,
+    'evento' =>$evento
 
 ]);
 }
--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -6,17 +6,88 @@
         <input type="text" class="formulario__input"
          id="nombre"
          name="nombre"
-         placeholder="Nombre Evento">
+         placeholder="Nombre Evento"
+         value='<?php echo $evento->nombre;?>'>
     </div>
 
     <div class="formulario__campo">
-        <label class='formulario__label' for="apellido">Descripción</label>
+        <label class='formulario__label' for="descripcion">Descripción</label>
         <textarea class="formulario__input"
          id="descripcion"
          name="descripcion"
          placeholder="Descripción Ponente"
-         rows = 8></textarea>
+         rows = 8> 
+         <?php echo htmlspecialchars($evento->descripcion); ?></textarea>
     </div>
+
+      <div class="formulario__campo">
+        <label class='formulario__label' for="categoria">Categoria o Tipo de Evento</label>
+       <select class='formulario__select' id='categoria' name='categoria_id'>
+        <option value="">-Seleccionar-</option>
+       <?php foreach($categorias as $categoria) { ?>
+
+            <option <?php echo ($evento->categoria_id === $categoria->id) ? 'selected' : ''?> value="<?php echo $categoria->id;?>"><?php echo $categoria->nombre;?></option>
+        <?php } ?>
+       </select>
+    </div>
+
+    <div class="formulario__campo">
+        <label class='formulario__label' for="categoria">Selecciona el día</label>
+
+        <div class="formulario__radio">
+<?php foreach($dias as $dia) { ?>
+
+    <label for="<?php echo strtolower($dia->nombre);?>"><?php echo $dia->nombre;?></label>
+
+        <input type="radio"
+        id='<?php echo strtolower($dia->nombre);?>'
+        name='dia'
+        value='<?php echo $dia->id;?>'>
+
+    <?php } ?>
+
+        </div>
+      
+    </div>
+
+   <div class="formulario__campo" id="horas">
+    <label for="" class="formulario__label">Seleccionar Horas</label>
+<ul class="horas">
+    <?php 
+    foreach($horas as $hora){ ?>
+
+    <li class='horas__hora'><?php echo $hora->hora;?></li>
+
+    <?php } ?>
+</ul>
+
+   </div>
+
+</fieldset>
+
+
+<fieldset class='formulario__fieldset'>
+
+<legend class="formulario__legend">Información Extra</legend>
+
+  <div class="formulario__campo">
+        <label class='formulario__label' for="ponentes">Ponentes</label>
+        <input type="text" class="formulario__input"
+         id="ponentes"
+         placeholder="Buscar Ponente">
+    </div>
+
+    <div class="formulario__campo">
+        <label class='formulario__label' for="ponentes">Lugares Disponibles</label>
+        <input type="number"
+         min='1'
+         class="formulario__input"
+         id="disponibles"
+         name = 'disponibles'
+         placeholder="Ej. 20"
+         value = '<?php echo $evento->disponibles?>'>
+    </div>
+
 
 </fieldset>
 
